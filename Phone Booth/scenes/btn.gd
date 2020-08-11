2da1432e426ea2755c8c13badb6e0c455a832df7
Yi Chen Hock <yichenhock@gmail.com>
2020-08-11 12:38:42 +0100

    Initial commit

---
+++ Phone Booth/scenes/btn.gd
@@ -0,0 +1,18 @@
+tool
+extends TextureButton
+
+export(Texture) var image setget set_image
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func set_image(new_image):
+	image = new_image
+	$symbol.texture = new_image
+	
+func button_down():
+	$symbol.rect_position.x += 1
+
+func button_up():
+	$symbol.rect_position.x -= 1
---
+++ README.md
@@ -0,0 +1,2 @@
+# Don't Hang Up
+ A small scope point and click horror game set in a phone booth. 
